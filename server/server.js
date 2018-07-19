c16aeb4282826d5541574c15982e99711c707e45
CHARFOUNI <abdelkarim.charfouni@capgemini.com>
2018-07-19 15:44:37 +0100

    add connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log("New user connected");
+    socket.on('disconnect',()=>{
+        console.log("User disconnected");
+    });
+});
 
 const publicPath = path.join(__dirname,'../public');
 app.use(express.static(publicPath));
@@ -9,6 +20,6 @@ app.get('/', (req, res)=>{
     res.send("<h1>Holla senior</h1>");
 });
 
-app.listen(3100, ()=>{
+server.listen(3100, ()=>{
     console.log("Server up on port 3100");
 });
